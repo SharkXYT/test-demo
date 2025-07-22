a285aada570078f7a7da74d997d9e4c92eba2ec4
Lcp <940898824@qq.com>
2025-07-22 11:47:22 +0800

    测试改动

--- demo.js
+++ demo.js
@@ -1,6 +1,12 @@
 window.onload = function() {
-    let box =document.getElementsByClassName('box')[0]
-    box.addEventListener('click', () => {
+    let box1 =document.getElementsByClassName('box1')[0]
+    box1.addEventListener('click', () => {
         alert('这是测试代码')
     }) 
 }
+window.onload = function() {
+    let box2 = document.getElementsByClassName('box2')[0]
+    box2.addEventListener('click', () => {
+        alert('这是第二行测试代码')
+    })
+}   
